0ee63e66d89d4ed07eeb8d30d4114bd952f3c5b8
Sergii Pryima <spryima@gmail.com>
2023-11-18 14:15:44 +0200

    migrations made

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True)
+    done = models.BooleanField(blank=True)
+    task = models.ManyToManyField(Tag, related_name="task")
+
+    class Meta:
+        ordering = ["-datetime"]
+
+    def __str__(self):
+        return f"{self.datetime} - {self.content}"
